72a6daa4abf1b52591980b6197cf195adf5c7b5a
Naxden <kilarjan05@gmail.com>
2023-05-30 17:30:12 +0200

    Initial commit

---
+++ src/main/java/com/example/klubfitnes/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.klubfitnes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
