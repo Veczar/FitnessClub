adb3af720177a298f6939985f4ba89773e901e8d
Naxden <kilarjan05@gmail.com>
2023-06-10 13:18:25 +0200

    Abstrakcyjna klasa Osoba

--- src/main/java/klubfitnes/Klient.java
+++ src/main/java/klubfitnes/Klient.java
@@ -1,38 +1,7 @@
 package klubfitnes;
 
-public class Klient {
-    private int id;
-    private String imie;
-    private String nazwisko;
-    private String telefon;
-
-
+public class Klient extends Osoba {
     public Klient(int id, String imie, String nazwisko, String telefon) {
-        this.id = id;
-        this.imie = imie;
-        this.nazwisko = nazwisko;
-        this.telefon = telefon;
-    }
-
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getImie() {
-        return imie;
-    }
-    public void setImie(String imie) { this.imie = imie; }
-
-    public String getNazwisko() {
-        return nazwisko;
-    }
-    public void setNazwisko(String nazwisko) { this.nazwisko = nazwisko; }
-
-    public String getTelefon() {
-        return telefon;
-    }
-    public void setTelefon(String telefon) { this.telefon = telefon; }
-
-    public String toString() {
-        return id + " " + imie + " " + nazwisko + " " + telefon;
+        super(id, imie, nazwisko, telefon);
     }
 }
